69cd517d1d870789b9f9e17e701a2564c0c7c100
Flamey0 <169044160+Flamey0@users.noreply.github.com>
2025-04-08 10:05:02 +0200

    .

--- app/src/main/java/com/example/projekt1_vidjaa/MainActivity.kt
+++ app/src/main/java/com/example/projekt1_vidjaa/MainActivity.kt
@@ -16,7 +16,7 @@ import com.example.projekt1_vidjaa.ui.theme.Projekt1_VidJašaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() 
         setContent {
             Projekt1_VidJašaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
